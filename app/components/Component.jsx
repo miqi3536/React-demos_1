dde39d05d45e97ae8aebba14bf5311c680848075
miqi3536 <miqi3536@163.com>
2017-10-07 13:45:47 +0800

    使用webpack + react 创建的一个demo程序 参照《从零开始，教你用Webpack构建React基础工程》（waka的http://www.jianshu.com/p/4df92c335617）

---
+++ app/components/Component.jsx
@@ -0,0 +1,13 @@
+//Component.jsx
+import React from 'react';
+
+class Component extends React.Component{
+    render(){
+        return (
+            <div>Hello World!</div>
+        )
+    }
+}
+
+//导出组件
+export default Component;
---
+++ app/main.js
@@ -0,0 +1,9 @@
+//main.js
+import React from 'react';
+import ReactDom from 'react-dom';
+import Component from './components/Component.jsx';
+
+ReactDom.render(
+    <Component/>,
+    document.getElementById('content')
+);
